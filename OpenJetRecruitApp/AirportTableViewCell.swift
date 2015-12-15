b7eaf57a5ac8e014094ac27910dd804e9f5765d7
akabab <ycribier@student.42.fr>
2015-12-15 23:51:53 +0100

    First display of fetched airports, with country name & continent matches

---
+++ OpenJetRecruitApp/AirportTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AirportTableViewCell.swift
+//  OpenJetRecruitApp
+//
+//  Created by Yoann Cribier on 15/12/15.
+//  Copyright © 2015 Yoann Cribier. All rights reserved.
+//
+
+import UIKit
+
+class AirportTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+
+    @IBOutlet weak var countryLabel: UILabel!
+}
