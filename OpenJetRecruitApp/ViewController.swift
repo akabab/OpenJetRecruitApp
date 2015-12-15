20d5724a90fa69f49ec081bf284c1e9765bdcd18
akabab <ycribier@student.42.fr>
2015-12-15 13:25:14 +0100

    Initial Commit

---
+++ OpenJetRecruitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenJetRecruitApp
+//
+//  Created by Yoann Cribier on 15/12/15.
+//  Copyright © 2015 Yoann Cribier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
